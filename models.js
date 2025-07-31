b309eba3227c59b09b97816d89fbfde4aeca159b
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-07-31 14:43:05 +1200

    Created Board schema

--- models.js
+++ models.js
@@ -3,23 +3,38 @@ const { Schema } = mongoose;
 
 const date = new Date();
 
+//Reply Schema
 const ReplySchema = new Schema({
     text: { type: String },
     delete_password: { type: String },
     created_on: { type: Date, default: date },
     bumped_on: { type: Date, default: date },
-    reported: { type: Boolean, default: false }
+    reported: { type: Boolean, default: false },
 });
 
 const Reply = mongoose.model("Reply", ReplySchema);
 
+
+//Thread Schema
 const ThreadSchema = new Schema({
     text: { type: String },
     delete_password: { type: String },
     created_on: { type: Date, default: date },
     bumped_on: { type: Date, default: date },
     reported: { type: Boolean, default: false },
-    replies: { type: [ReplySchema] }
+    replies: { type: [ReplySchema] },
 });
 
 const Thread = mongoose.model("Thread", ThreadSchema);
+
+//Board Schema
+const BoardSchema = new Schema({
+    name: { type: String},
+    threads: { type: [ThreadSchema] },
+});
+
+const Board = mongoose.model("Board", BoardSchema);
+
+exports.board = Board;
+exports.thread = Thread;
+exports.reply = Reply;
