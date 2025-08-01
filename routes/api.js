0fc0c63e3c58704ec01daebaa6e97e15e19046aa
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-08-01 14:12:55 +1200

    Working on creating new thread using post. Created a function to create a new therad and creating functions to create a new board

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,50 @@
 'use strict';
+
+const { board } = require('../models');
+
 const BoardModel = require('../models').Board;
 const ThreadModel = require('../models').Thread;
 const ReplyModel = require('../models').Reply;
 
+async function newEmptyThread(txt, delete_pass) {
+  return new ThreadModel({
+    text: txt,
+    delete_password: delete_pass,
+    replies: [],
+  });
+}
+
+async function createEmptyBoard(board) {
+  return new BoardModel({
+    name: board,
+    threads: [],
+  });
+}
+
+function findBoard(board, thread) {
+  BoardModel.findOne({name: board}, async (err, Boardata) =>{
+    if(!BoardData){
+      const newBoard = await createEmptyBoard(board);
+    }
+  });
+}
+
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+  .post(async (req, res) => {
+    const { text, delete_password } = req.body;
+    let board = req.body.board;
+    if(!board){board = req.params.board;}
+    console.log(req.body);
+
+    //Creating a new thread
+    const newThread = await newEmptyThread(text, delete_password);
+    console.log("New Thread", newThread)
+
+    findBoard(board, newThread);
+
+  });
     
   app.route('/api/replies/:board');
 
