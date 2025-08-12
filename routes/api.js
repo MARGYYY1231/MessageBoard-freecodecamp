5513c9551e271012772a80f1cde53805bba76c68
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-08-12 14:20:41 +1200

    Saved board in findboard and added missing await

--- routes/api.js
+++ routes/api.js
@@ -56,7 +56,7 @@ function findBoard(board, newThread, res) {
   BoardModel.findOne({name: board}, async (err, Boarddata) =>{
     if(!Boarddata){
       const newBoard = await createEmptyBoard(board);
-      newBoard.save();
+      await newBoard.save();
       console.log("newBoard", newBoard)
 
       saveBoard(newBoard, newThread, res);
