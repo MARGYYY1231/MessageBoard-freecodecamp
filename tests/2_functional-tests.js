f2709402d9ac9a8ff57566ed2ee934a5d40e60a2
MARGYYY1231 <138099922+MARGYYY1231@users.noreply.github.com>
2025-08-13 16:07:10 +1200

    Created first test to test creating a new thread using post

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,21 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+    let testBoard = 'testboard';
+    let testThreadId;
+    let testReplyId;
+    let testThreadPassword = 'threadpass';
+    let testReplyPassword = 'replypass';
 
+    test('Create a new thread on board "testboard"', function(done){
+        chaiHttp.request(server)
+        .post(`/api/threads/${testBoard}`)
+        .send({ text: 'Test thread', delete_password: testThreadPassword })
+        .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.property(res.body, '_id');
+            assert.equal(res.body.text, 'Test thread');
+            done();
+        });
+    });
 });
